56ea72ab97e2e5c9324698bb2fce13c05d7f11d8
Tranquil-M <bpihwhshmh@gmail.com>
2026-02-24 00:06:00 -0800

    added back post-install script

---
+++ stow/.config/hypr/conf.d/post-install.sh
@@ -0,0 +1,9 @@
+echo "Applying Hyprland Colorscheme..."
+cd "$HOME"
+if command -v matugen >/dev/null 2>&1; then
+  matugen image "${wall_dir}/${wall_selection}" --source-color-index 0 || matugen image "${wall_dir}/${wall_selection}"
+  swww img --resize crop --transition-type center "${wall_dir}/${wall_selection}"
+fi
+gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3'
+
+rm -- "$0"
